7f43b997f2b7104c5bd9e0382eecc0be8d3e709f
hgsgtk <hgsgtk@gmail.com>
2020-09-06 10:54:34 +0900

    style: tffmt

--- raw-instance/main.tf
+++ raw-instance/main.tf
@@ -8,16 +8,16 @@ provider "google" {
 
 # doc: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance
 resource "google_compute_instance" "default" {
-  name          = var.name
-  machine_type  = "n1-standard-2"
-  zone          = "asia-northeast1-b"
-  tags = ["tag", "isucon10"]
+  name         = var.name
+  machine_type = "n1-standard-2"
+  zone         = "asia-northeast1-b"
+  tags         = ["tag", "isucon10"]
   boot_disk {
     auto_delete = true
     initialize_params {
       image = "ubuntu-os-cloud/ubuntu-1804-lts"
-      size = 20
-      type = "pd-standard"
+      size  = 20
+      type  = "pd-standard"
     }
   }
 
