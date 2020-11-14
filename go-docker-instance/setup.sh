1875e4e5e37722469097a14beb5c8c551e11e837
中村 弘武 <abctail30@gmail.com>
2020-11-14 14:37:29 +0900

    v u@

--- go-docker-instance/setup.sh
+++ go-docker-instance/setup.sh
@@ -12,9 +12,9 @@ curl -fsSL get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo docker version
 
-# install go 1.14.7
-wget https://dl.google.com/go/go1.14.7.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.14.7.linux-amd64.tar.gz
+# install go
+wget https://dl.google.com/go/go1.15.1.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.15.1.linux-amd64.tar.gz
 # # use usr/local/go/bin PATH
 # echo 'export PATH=$PATH:/usr/local/go/bin' >> /home/$USER/.profile
 # source ~/.profile
